dd95bcd1275b08272bee79343bd76f0d74bc525f
LJ O <ljoks0727@gmail.com>
2019-03-29 17:18:59 +0000

    adds websocket skeleton

--- backend/routes/socket.js
+++ backend/routes/socket.js
@@ -2,5 +2,38 @@ module.exports = (io) => {
  io.on('connection', client =>
  {
      console.log('someone connected');
+
+     //recieved from mobile, sent to browser
+     //client can join if session has less than max users
+     client.on('join', (sessionCode, user) =>
+     {
+
+     });
+
+     //recieved from browser
+     //indicates that no more players can join
+     client.on('sessionStart', (sessionCode) =>
+     {
+
+     });
+
+    //recieved from mobile, sent to browser
+     client.on('castHook', user =>
+     {
+
+     });
+
+     //move the users reel up or down depending on the direction they reel
+     //recieved from mobile, sent to browser
+     client.on('reel', (user, direction) =>
+     {
+
+     });
+
+     //recieved from browser only, then sent to phone
+     client.on('fishCaught', (user, fish) =>
+     {
+
+     });
  });
 };
