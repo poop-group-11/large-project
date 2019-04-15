966ed649ed97616643e23c88326658939441ee85
Pradheep Kethi-Reddy <pradheep.kethireddy@knights.ucf.edu>
2019-04-15 12:43:25 -0400

    Update script.js
    Script.js now interfaces with API and creates and manages a web socket properly.

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,11 @@
+var connection;
+var sessionId;
+
 function startGame() {
   document.getElementById("start").style.display = 'none';
   document.getElementById("back").style.display = 'none';
+  //Start this bad boy up yeyeyeyeyeye
+  connection.emit('sessionStart', sessionId);
   draw();
 }
 
@@ -9,6 +14,36 @@ function newSession() {
   document.getElementById("game-screen").style.display = 'grid';
   document.getElementById("settings-menu").style.display ='none';
   document.getElementById("game-music").play();
+  //Open socket, listen for join, make hook appear.
+  connection = io('https://poopgroup11.xyz/leins');
+  fetch('https://poopgroup11.xyz/api/openSession', { method: "POST" })
+	.then(res = > res.json())
+	.then
+	( 
+		if(res.success == true)
+		{
+			sessionId = res.message;
+			var userCount = 0;
+			connection.emit('joinSession', sessionId);
+			connection.on('browserJoined' => {console.log('successful')});
+			//TODO: Display the id in the CSS
+			document.getElementById("session-code").innerHTML = sessionId;
+			//Listen for people to join
+			connection.on('userJoined', (user) =>
+			{
+				//Pass the info on to display it.
+				displayUser(user.username, userCount);
+				userCount++;
+			});
+		}
+		else
+		{
+			//TODO: Display an error
+			//Bring up back to the homescreen.
+			backToMenu();
+		}
+	)
+	.catch( err => console.log(err);) ;
 }
 
 function backToMenu() {
