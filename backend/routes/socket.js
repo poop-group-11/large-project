9f81125c42732d870284d3ecc1825e2d00dc6e45
LJ O <ljoks0727@gmail.com>
2019-04-24 04:19:32 +0000

    reconnect log

--- backend/routes/socket.js
+++ backend/routes/socket.js
@@ -72,6 +72,7 @@ module.exports = (io) => {
      });
 
      client.on('reconnect', (data) => {
+       console.log("reconnecting to " + sessionCode.toUpperCase());
        client.join(data.sessionCode.toUpperCase());
      });
 
